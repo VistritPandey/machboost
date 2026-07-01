4d3a0b3ca7dc106429208d9a572d3f78ec9bf3ba
Vistrit <vistritpandey@gmail.com>
2026-07-01 06:35:01 -0500

    Export benchmark API

--- machboost/__init__.py
+++ machboost/__init__.py
@@ -1,4 +1,13 @@
 from .accelerator import Accelerator, AcceleratorResult
+from .bench import (
+    BenchmarkCase,
+    BenchmarkResult,
+    GateDecision,
+    GatePolicy,
+    benchmark,
+    benchmark_cases,
+    summarize_results,
+)
 from .core import (
     BoostedService,
     Candidate,
@@ -11,10 +20,17 @@ from .core import (
 __all__ = [
     "Accelerator",
     "AcceleratorResult",
+    "BenchmarkCase",
+    "BenchmarkResult",
     "BoostedService",
     "Candidate",
     "CorpusDrafter",
+    "GateDecision",
+    "GatePolicy",
     "MachBoost",
     "RunStats",
+    "benchmark",
+    "benchmark_cases",
     "machboost",
+    "summarize_results",
 ]
