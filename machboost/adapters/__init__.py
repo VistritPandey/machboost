4fb4286cca29664a61c1cba4ac6023330b2e1fbb
Vistrit <vistritpandey@gmail.com>
2026-06-30 20:37:01 -0500

    Export Ollama adapter

--- machboost/adapters/__init__.py
+++ machboost/adapters/__init__.py
@@ -1,8 +1,13 @@
 from .huggingface import HuggingFaceCausalLMService, Verification
 from .mlx import MLXCausalLMService
+from .ollama import OllamaCapabilities, OllamaGenerateResult, OllamaHTTPAdapter, OllamaHTTPError
 
 __all__ = [
     "HuggingFaceCausalLMService",
     "MLXCausalLMService",
+    "OllamaCapabilities",
+    "OllamaGenerateResult",
+    "OllamaHTTPAdapter",
+    "OllamaHTTPError",
     "Verification",
 ]
