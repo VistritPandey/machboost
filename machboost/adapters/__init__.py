83474c06cac5ba4531e423f60c9621e11df47e37
Vistrit <vistritpandey@gmail.com>
2026-07-14 13:53:50 -0500

    feat: publish MLX vision adapter

--- machboost/adapters/__init__.py
+++ machboost/adapters/__init__.py
@@ -1,13 +1,16 @@
 from .huggingface import HuggingFaceCausalLMService, Verification
 from .mlx import MLXCausalLMService
+from .mlx_vlm import MLXVLMAccelerator, VisionRunStats
 from .ollama import OllamaCapabilities, OllamaGenerateResult, OllamaHTTPAdapter, OllamaHTTPError
 
 __all__ = [
     "HuggingFaceCausalLMService",
     "MLXCausalLMService",
+    "MLXVLMAccelerator",
     "OllamaCapabilities",
     "OllamaGenerateResult",
     "OllamaHTTPAdapter",
     "OllamaHTTPError",
     "Verification",
+    "VisionRunStats",
 ]
